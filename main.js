b007c22cb1facc072bbac9ce04b8b8df2152fe94
Dabsia <dabojohnson98@gmail.com>
2022-02-05 19:39:16 +0000

    An App that shows an overview of a movie that is collected from themoviedb API

---
+++ main.js
@@ -0,0 +1,70 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=cf103a8369add6dd90594a095c18616f&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?&api_key=cf103a8369add6dd90594a095c18616f&query=""'
+
+let form =document.getElementById('form');
+let search =document.getElementById('search');
+let main = document.getElementById('main')
+
+// getInitial movies
+
+getMovies(API_URL)
+async function getMovies(url){
+    const res = await fetch(url)
+    const data = await res.json()
+
+    showMovies(data.results)
+}
+
+const showMovies = (movies) => {
+    main.innerHTML = '';
+
+    movies.forEach(movie =>{
+        const {title,  poster_path, vote_average, overview} = movie;
+        const movieEl = document.createElement('div')
+        console.log(poster_path)
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+        <img src="${IMG_PATH + poster_path}" alt="${title}" >
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                <p>${overview}</p>
+            </div>
+        `
+        main.appendChild(movieEl)
+    })
+}
+
+function getClassByRate(vote){
+    if (vote >= 8){
+        return 'green'
+    }
+    else if (vote >= 5 ){
+        return 'orange'
+    }
+    else{
+        return 'red'
+    }
+}
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value;
+
+    if(searchTerm && searchTerm !== ''){
+        getMovies(SEARCH_URL + searchTerm)
+
+        search.value = ''
+    }
+    else{
+        window.location.reload()
+    }
+
+})
